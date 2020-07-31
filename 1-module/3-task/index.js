9d57217fe395e2a8d2aea7b6fd1de4d94f1b210c
Detrimon <detrimon@gmail.com>
2020-07-31 13:12:44 +0300

    Занятие 1: Задание 3: Сделать первую букву заглавной

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,5 +4,18 @@
  * @returns {string}
  */
 function ucFirst(str) {
-  // ваш код...
+  // Либо можно вначале проверить на наличие строки. И если строка пустая, то сразу вернуть эту строку,
+  // Так лишнюю операцию по сплиту делать не придется.
+  // if (!str) {
+  //   return str;
+  // }
+
+  let aStr = str.split('');
+
+  if (aStr[0]) {
+    aStr[0] = aStr[0].toUpperCase();  
+    return aStr.join('');
+  }
+
+  return str;  
 }
