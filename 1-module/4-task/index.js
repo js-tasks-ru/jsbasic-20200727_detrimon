3e9dae658ae585d3287aafd93f6ff23d6d1e0bf0
Detrimon <detrimon@gmail.com>
2020-07-31 21:34:09 +0300

    Занятие 1: Задача 4: Спам-фильтр Решение двумя способами: без RegExp и с RegExp.

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,12 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+
+  // Без использования RegExp
+  str = str.toLowerCase();
+  return !(str.indexOf('1xbet') === -1) || !(str.indexOf('xxx') === -1);
+
+  // С использованием RegExp::
+  // let regStr = /1xbet|xxx/i;
+  // return regStr.test(str) || false;
 }
