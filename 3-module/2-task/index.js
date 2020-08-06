aaf2916575fbf36eb44dd60fa5943179a5dff9f3
Detrimon <detrimon@gmail.com>
2020-08-06 22:27:27 +0300

    Занятие 3: Функции и массивы Задача 2: Фильтрация по диапазону

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -5,5 +5,17 @@
  * @returns {number[]}
  */
 function filterRange(arr, a, b) {
-  // ваш код...
+  // Решение с методом filter (после занятия :)))
+  return arr.filter(elem => elem >= a && elem <= b);
+
+  // Решение без метода filter (делал до занятия)
+  // let aFiltered = [];
+
+  // for (let elem of arr) {
+  //   if (elem >= a && elem <= b) {
+  //     aFiltered.push(elem);
+  //   }
+  // }
+
+  // return aFiltered;
 }
