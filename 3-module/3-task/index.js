79e006789c5382ad207cb089381fb469e014b983
Detrimon <detrimon@gmail.com>
2020-08-06 22:39:07 +0300

    Занятие 3: Функции и массивы Задание 3: Переведите текст вида border-left-width в borderLeftWidth

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,23 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  // Решение с RegExp ::
+  return str.replace(/(\w*)(-\w)*/ig,
+    function(full, s1 = '', s2 = '') {
+      return `${s1}${s2.toUpperCase()}`;
+    }).split('-').join('');
+
+  // Решение без RegExp ::
+  // return str.split('-').map((sElem, index) => {
+  //   if (index === 0) {
+  //     return sElem;
+  //   }
+
+  //   let aElem;
+  //   aElem = sElem.split('');
+  //   aElem[0] = aElem[0].toUpperCase();
+  //   sElem = aElem.join('');
+
+  //   return sElem;
+  // }).join('');
 }
