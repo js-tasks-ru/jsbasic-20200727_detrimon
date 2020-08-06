86c662f1d3d5fbbe7f903ef0e3bec4c7fc7ce075
Detrimon <detrimon@gmail.com>
2020-08-07 00:19:52 +0300

    Занятие 3: Функции и массивы Задача 5: Найти min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,15 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  // ваш код...
+
+  // Долго допирал, как сделать без RegExp, но со split :) stackOverflow помог :) Наверное, так..
+  let aNums = str.split(',').join(' ').split(' ')
+                .filter(elem => parseFloat(elem))
+                .map(elem => parseFloat(elem));
+
+  // А это вариант с RegExp ::
+  // let aNums = str.match(/-*\d+\.*\d*/g)
+  //                 .map(elem => parseFloat(elem));
+
+  return { min: Math.min(...aNums), max: Math.max(...aNums) };
 }
