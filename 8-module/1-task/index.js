cf75e22a8bf01a53d93e0a0b1d25e9c43b31d342
Detrimon <detrimon@gmail.com>
2020-08-29 00:54:07 +0300

    Занятие 8: Взаимодействие с сервером Учебный проект: Иконка корзины

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -39,6 +39,31 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+    if (this.elem.offsetWidth === 0) {
+      return;
+    }
+
+    if (document.documentElement.clientWidth <= 767) {
+      Object.assign(this.elem.style, {
+        zIndex: '',
+        position: '',
+        top: '',
+        left: '',
+      });
+    } else {
+      Object.assign(this.elem.style, {
+        zIndex: 1000,
+        position: 'fixed',
+        top: '50px'
+      });
+
+      let oFirstContainer = document.querySelector('.container');
+
+      let leftIndent = Math.min(
+        document.documentElement.clientWidth - this.elem.offsetWidth - 10,
+        oFirstContainer.getBoundingClientRect().right + 20);
+
+      this.elem.style.left = `${leftIndent}px`;
+    }
   }
 }
